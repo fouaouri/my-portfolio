fcb4fa1c4d42fd2307c2ea4e238c9be9c05055aa
fouaouri <fzouaourikt@gmail.com>
2025-01-21 00:31:20 +0100

    res problems

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,9 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
+import logo from "../assets/logo.png";
 
 import { Link } from "react-router-dom";
-import {
-  AiFillStar,
-  AiOutlineHome,
-  AiOutlineFundProjectionScreen,
-  AiOutlineUser,
-} from "react-icons/ai";
-
-import { CgFileDocument } from "react-icons/cg";
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
@@ -53,7 +45,6 @@ function NavBar() {
           <Nav className="ms-auto" defaultActiveKey="#home">
             <Nav.Item>
               <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-                {/* <AiOutlineHome style={{ marginBottom: "2px" }} />  */}
                 Home
               </Nav.Link>
             </Nav.Item>
@@ -64,7 +55,6 @@ function NavBar() {
                 to="/about"
                 onClick={() => updateExpanded(false)}
               >
-                {/* <AiOutlineUser style={{ marginBottom: "2px" }} />  */}
                 About
               </Nav.Link>
             </Nav.Item>
@@ -75,22 +65,9 @@ function NavBar() {
                 to="/project"
                 onClick={() => updateExpanded(false)}
               >
-                {/* <AiOutlineFundProjectionScreen
-                  style={{ marginBottom: "2px" }}
-                />{" "} */}
                 Projects
               </Nav.Link>
             </Nav.Item>
-
-            {/* <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item> */}
 
           </Nav>
         </Navbar.Collapse>
